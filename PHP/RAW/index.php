a509acecedcf608a2c409c448262f2a22bafa4f0
Raju <voiceofemotion321@gmail.com>
2024-02-19 16:31:57 +0600

    This is few changes

---
+++ PHP/RAW/index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$a = 10;
+$b = 23;
+$c = 26;
+
+if($a % 2 == 22){
+    echo "Even ba Jor Sonkha";
+
+}else{
+    echo " Odd ba Bijor Sonkha";
+}
+
+// $raju = 12;
+// $rakib = 13;
+// $jakir = 14;
+
+// if(($raju==12) && ($jakir == 15)){
+//     echo " They are same";
+// }elseif($jakir == 16){
+//     echo " It'e the alternative thing";
+// }elseif($rakib == 13){
+//     echo"Rakib is a gay";
+// }
+// else{
+//     echo " they are not same thing";
+// }
+
+// Let's Learn Ternary Operator
+
